29920b028fd664ccb2f9a23a2ddd26acdda9fe05
Stasha13 <nastassja1301@gmail.com>
2019-06-13 15:43:55 +0100

    Java

--- src/main/java/Nationwide/Add/JDBC2.java
+++ src/main/java/Nationwide/Add/JDBC2.java
@@ -9,7 +9,7 @@ public class JDBC2 {
 		Statement stmt = null;
 	try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/nationwide?useLegacyDatetimeCode=false&serverTimezone=Europe/London","root","Jhb4534586");
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/nationwide?useLegacyDatetimeCode=false&serverTimezone=Europe/London","root","123456");
 		stmt = conn.createStatement();
 		//stmt.executeUpdate("insert into school values(1,'Stasha',80)");
 		ResultSet rs = stmt.executeQuery("Select * from school");
